104992f9aced8f5ed04ca8c51b92388b3aa1a0ef
Andrew <apetruska@mines.edu>
2021-01-22 14:32:43 -0700

    updated to install python packages use apt and added .bin path to path so pip installed packages can be found

--- SerialMonitor/install_dep.sh
+++ SerialMonitor/install_dep.sh
@@ -1,1 +1,6 @@
-python3 -m pip install pandas pyserial matplotlib
+sudo apt-get install -y python3 libatlas-base-dev python3-numpy python3-pandas python3-serial python3-matplotlib 
+
+echo "export Path=/home/pi/.local/bin:$Path" >> ~/.bashrc
+. ~/.bashrc
+
+#python3 -m pip install pandas pyserial matplotlib
