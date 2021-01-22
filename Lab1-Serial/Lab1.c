71d5ae4e93504e6e8ed377d02b3db8501e0013d1
Andrew Petruska <apetruska@mines.edu>
2021-01-21 23:03:26 -0700

    First commit of Lab1 files

---
+++ Lab1-Serial/Lab1.c
@@ -0,0 +1,16 @@
+#include "SerialIO.h"
+
+/** Main program entry point. This routine configures the hardware required by the application, then
+ *  enters a loop to run the application tasks in sequence.
+ */
+int main(void)
+{
+    USB_SetupHardware();
+    GlobalInterruptEnable();
+
+    for (;;)
+    {
+        USB_Echo_Task();
+        USB_USBTask();
+    }
+}
