5f01abb3d3a31197e2268aff35a76175d9653dc6
Andrew Petruska <apetruska@mines.edu>
2021-01-25 20:57:01 -0700

    updated include statement to explicitly provide the location of SerialIO.h so it plays nicely with the clion IDE

--- Lab1-Serial/Lab1.c
+++ Lab1-Serial/Lab1.c
@@ -1,4 +1,4 @@
-#include "SerialIO.h"
+#include "../c_lib/SerialIO.h"
 
 /** Main program entry point. This routine configures the hardware required by the application, then
  *  enters a loop to run the application tasks in sequence.
