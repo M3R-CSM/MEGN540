72131a0e2969e2c70eda5a3153a10e7606cf206e
Andrew <apetruska@mines.edu>
2021-03-26 10:34:15 -0600

    Added Controller.h/c to the repo to support Lab 5

---
+++ c_lib/Controller.h
@@ -0,0 +1,93 @@
+/*
+         MEGN540 Mechatronics Lab
+    Copyright (C) Andrew Petruska, 2021.
+       apetruska [at] mines [dot] edu
+          www.mechanical.mines.edu
+*/
+
+/*
+    Copyright (c) 2021 Andrew Petruska at Colorado School of Mines
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+*/
+
+/**
+ * Controller.h/c defines the functions necessary to implement a z-transform
+ * based control of the forward angular and linear speed of the car. 
+ * 
+ * This code leverages the filter.h/c code developed in the homework. 
+ *
+ */
+#ifndef _MEGN540_CONTROLLER_H
+#define _MEGN540_CONTROLLER_H
+
+#include "Filter.h"
+
+typedef struct { Filter_Data_t controller; float kp; float target_pos; float target_vel; float update_period;} Controller_t;
+
+/**
+ * Function Saturate saturates a value to be within the range.
+ */
+inline float Saturate( float value, float ABS_MAX )
+{
+    return (value > ABS_MAX)?ABS_MAX:(value < -ABS_MAX)?-ABS_MAX:value;
+}
+
+/**
+ * Function Initialize_Controller setsup the z-transform based controller for the system.
+ */
+void Controller_Init(Controller_t* p_cont, float kp, float* num, float* den, uint8_t order, float update_period);
+
+/**
+ * Function Controller_Set_Target_Velocity sets the target velocity for the 
+ * controller.
+ */
+void Controller_Set_Target_Velocity( Controller_t* p_cont, float vel );
+
+/**
+ * Function Controller_Set_Target_Position sets the target postion for the 
+ * controller, this also sets the target velocity to 0.
+ */
+void Controller_Set_Target_Position( Controller_t* p_cont, float vel );
+
+/**
+ * Function Controller_Update takes in a new measurement and returns the
+ * new control value.
+ */
+float Controller_Update( Controller_t* p_cont, float measurement, float dt );
+
+/**
+ * Function Controller_Last returns the last control command
+ */
+float Controller_Last( Controller_t* p_cont);
+
+/**
+ * Function Controller_SettTo sets the Filter's input and output lists
+ * to match the measurement so it starts with zero error.
+ */
+void Controller_SetTo(Controller_t* p_cont, float measurement );
+
+/**
+ * Function Controller_ShiftBy shifts the Filter's input and output lists
+ * by the desired amount. This is helpful when dealing with wrapping.
+ */
+void Controller_ShiftBy(Controller_t* p_cont, float measurement );
+
+#endif
