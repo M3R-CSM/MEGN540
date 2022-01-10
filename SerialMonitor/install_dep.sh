220725f7cf7db1bbb527f73e4b1e7b8491d217fa
Andrew Petruska <apetruska@mines.edu>
2022-01-10 13:54:49 -0700

    added tikinter to python install dependencies

--- SerialMonitor/install_dep.sh
+++ SerialMonitor/install_dep.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y python3 libatlas-base-dev python3-numpy python3-pandas python3-serial python3-matplotlib 
+sudo apt-get install -y python3 libatlas-base-dev python3-numpy python3-pandas python3-serial python3-matplotlib python3-tk
 
 echo "export Path=/home/pi/.local/bin:$Path" >> ~/.bashrc
 . ~/.bashrc
